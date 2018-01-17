f9c15aaa1cbd59d421174344f152a12b52754e21
ElijahButers <Elijahbuters@gmail.com>
2018-01-17 18:32:42 +0200

    Add a new property with restaraunts’ names to RestaurantTableViewController class.

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class RestaurantTableViewController: UITableViewController {
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "Thai Cafe"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
