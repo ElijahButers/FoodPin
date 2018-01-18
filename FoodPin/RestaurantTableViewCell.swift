cbbf6d5fe32a9e515ba2a2aa17e2ed16206cc557
ElijahButers <Elijahbuters@gmail.com>
2018-01-18 20:58:36 +0200

    Add a new table view cell class as cocoa touch class named RestaurantTableViewCell.

---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by User on 1/18/18.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
