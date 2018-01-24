b663c5bb5e0d81b8928796e91802fe8ee12ab6db
ElijahButers <Elijahbuters@gmail.com>
2018-01-24 16:44:11 +0200

    Add code to viewDidLoad() method in RestaurantDetailViewController class that sets the image view's image to the selected restaurant image.

--- FoodPin/RestaurantDetailViewController.swift
+++ FoodPin/RestaurantDetailViewController.swift
@@ -18,6 +18,7 @@ class RestaurantDetailViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        restaurantImageView.image = UIImage(named: restaurantImage)
     }
 
     override func didReceiveMemoryWarning() {
