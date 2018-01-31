281ea884a6ea4de9eff438523172e85a8823e753
ElijahButers <Elijahbuters@gmail.com>
2018-01-31 21:42:25 +0200

    Add code to the viewDidLoad() in RestaurantDetailViewController class that changes table background to light gray, set the footer of the table view to blank, changes the color of the separators for content rows.

--- FoodPin/RestaurantDetailViewController.swift
+++ FoodPin/RestaurantDetailViewController.swift
@@ -20,6 +20,10 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
 
         // Do any additional setup after loading the view.
         restaurantImageView.image = UIImage(named: restaurant.image)
+        
+        tableView.backgroundColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, alpha: 0.2)
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
+        tableView.separatorColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/250.0, alpha: 0.2)
     }
 
     override func didReceiveMemoryWarning() {
