fb186dddae8e156db9779f6ddf4183c6cfc33f98
ElijahButers <Elijahbuters@gmail.com>
2018-02-05 13:25:06 +0200

    Update the restautants property in RestaurantTableViewController class that has a new phone parameter.

--- FoodPin/Restaurant.swift
+++ FoodPin/Restaurant.swift
@@ -16,12 +16,12 @@ class Restaurant {
     var image = ""
     var isVisited = false
     
-    init(name: String, type: String, location: String, image: String, phone: String, isVisited: Bool) {
+    init(name: String, type: String, location: String, phone: String, image: String, isVisited: Bool) {
         self.name = name
         self.type = type
         self.location = location
-        self.image = image
         self.phone = phone
+        self.image = image
         self.isVisited = isVisited
         
     }
