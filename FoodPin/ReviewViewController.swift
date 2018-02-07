3dc05b2fa8cb5188cbc3690056d8d2a45be3eeac
ElijahButers <Elijahbuters@gmail.com>
2018-02-07 10:25:32 +0200

    Connect the image view on the review view controller to the ReviewViewController class as outlet property.

--- FoodPin/ReviewViewController.swift
+++ FoodPin/ReviewViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ReviewViewController: UIViewController {
 
+    @IBOutlet var backgroundImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
