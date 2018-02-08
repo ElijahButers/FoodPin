1de21ab39289b3a529b03a76a1d69807a4849317
ElijahButers <Elijahbuters@gmail.com>
2018-02-08 17:37:07 +0200

    Update code in viewDidLoad() method in the ReviewViewController class that moves the container view off screen.

--- FoodPin/ReviewViewController.swift
+++ FoodPin/ReviewViewController.swift
@@ -22,7 +22,7 @@ class ReviewViewController: UIViewController {
         blurEffectView.frame = view.bounds
         backgroundImageView.addSubview(blurEffectView)
         
-        containerView.transform = CGAffineTransform.init(scaleX: 0, y: 0)
+        containerView.transform = CGAffineTransform.init(translationX: 0, y: -1000)
     }
 
     override func didReceiveMemoryWarning() {
